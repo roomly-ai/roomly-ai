d09f76252ad4c182f833688b5601ac3f44a020ea
roomly-ai <roomly.bnb@gmail.com>
2024-12-03 15:01:22 +0000

    my changes

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ const pool = require('./db')
 const port = 1337
 
 const app = express()
+app.use(express.json())
 
+//routes
 app.get('/', (req, res) => {
     res.sendStatus(200)
 })
+
+app.post('/', (req,))
 
 app.listen(port, () => console.log(`Server has started on port: ${port}`))
